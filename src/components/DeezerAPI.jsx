31ee86560e4c444e053942e924919182bfe43bb3
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-04-29 14:17:51 +0200

    Fetching Deezer API

--- src/components/DeezerAPI.jsx
+++ src/components/DeezerAPI.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useEffect } from "react";
 
-const DeezerAPI = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const DeezerAPIExample = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/12345/"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data.data);
 
-export default DeezerAPI
+      } catch (error) {
+        console.error("Error fetching tracks:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <div></div>;
+};
+
+export default DeezerAPIExample;
