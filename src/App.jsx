efd12d27b34618f9323b8621b53043c8b9b923b1
Mey-Lin Mus <mey-lin.mus@student.ehb.be>
2024-05-04 19:25:12 +0200

    Spotify API auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import DeezerAPI from "./components/DeezerAPI";
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
+const code = new URLSearchParams(window.location.search).get("code");
+
 function App() {
-  return (
-    <div>
-      <DeezerAPI />
-    </div>
-  );
+  return code ? <Dashboard code={code} /> : <Login />;
 }
 
 export default App;
